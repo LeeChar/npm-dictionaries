d371cf1d786158cca4d3349c3de881fb3b6c2daf
李昌南 <lichangnan@lichangnandeMacBook-Pro.local>
2019-11-04 06:02:27 +0800

    初版简易搜索功能完成

---
+++ data.js
@@ -0,0 +1,13 @@
+const data =  [
+  {
+    package: "inquirer",
+    desc: "运行命令时，提供命令行提问的输出输入交互",
+    key: ["命令行", "命令行提问"]
+  },
+  {
+
+    package: "readline",
+    desc: "一行行的读取文件内容",
+    key: ["一行行读取文件","一行一行的读"]
+  }
+]
