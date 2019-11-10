df4ae54f7f3d48e920e3ab6bfaed5f85da9d610d
李昌南 <lichangnan@lichangnandeMacBook-Pro.local>
2019-11-11 06:48:42 +0800

    add

--- data.js
+++ data.js
@@ -8,5 +8,15 @@ const data = [
     package: "readline",
     desc: "一行行的读取文件内容",
     key: ["一行行读取文件", "一行一行的读"]
+  },
+  {
+    package: "amfe-flexible",
+    desc: "给html加font-size，配合rem使用",
+    key: ["移动端适配", "rem"]
+  },
+  {
+    package: "px2rem-loader",
+    desc: "将px单位自动转换成rem",
+    key: ["loader", "移动端适配", "rem"]
   }
 ]
